70af23da168efce33467b087ba24edf8ecdc6929
cympyc1785 <cympyc1785@gmail.com>
2024-01-14 12:38:32 +0900

    FTR: 투표 시작 버튼 placeholder 추가

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography, Grid, Button } from '@mui/material';
 
 import History from '../../models/history';
 import logo from '../../images/logo192.png';
 import HistoryCard from '../../components/History';
 
 export function HistoryPage() {
+  const navigate = useNavigate();
   const [histories, setHistories] = useState<History[]>([
     {
       id: 2,
@@ -38,17 +40,29 @@ export function HistoryPage() {
   }, []);
 
   return (
-    <Box paddingX={3} paddingY={5}>
-      <Box paddingX={3} paddingY={3}>
-        <Typography variant="h4">히스토리</Typography>
+    <Box>
+      <Box paddingX={3} paddingY={5}>
+        <Box paddingX={3} paddingY={3}>
+          <Typography variant="h4">히스토리</Typography>
+        </Box>
+        <Grid container spacing={4} paddingX={3}>
+          {histories.map((history) => (
+            <Grid item xs={6} sm={4} md={3} lg={2} xl={1.5}>
+              <HistoryCard navDir="/" imgDir={logo} pollName={history.pollName} endedAt={history.endedAt} />
+            </Grid>
+          ))}
+        </Grid>
       </Box>
-      <Grid container spacing={4} paddingX={3}>
-        {histories.map((history) => (
-          <Grid item xs={6} sm={4} md={3} lg={2} xl={1.5}>
-            <HistoryCard navDir="/" imgDir={logo} pollName={history.pollName} endedAt={history.endedAt} />
-          </Grid>
-        ))}
-      </Grid>
+      <Button
+        style={{
+          position: 'fixed',
+          bottom: '16px', // 조정 가능한 값
+          right: '16px', // 조정 가능한 값
+        }}
+        onClick={() => navigate('/')}
+      >
+        투표 시작하기
+      </Button>
     </Box>
   );
 }
