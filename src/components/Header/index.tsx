ab0256038aec2b46d5fbe7c07e795484f2e1c9bc
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 <sue.gus77@gmail.com>
2024-01-14 22:50:22 +0900

    MOD: 로그인 상태에 따른 버튼 갱신

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,31 @@
-// import { useNavigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Button from '@mui/material/Button'; // Import Button component from @mui/material package
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar'; // Import AppBar component from @mui/material package
 import { Toolbar, Typography } from '@mui/material';
 
 import SideBar from '../Sidebar';
+import { useAuth } from '../AuthContent';
 
 export default function Header() {
+  const { isLoggedIn, logout } = useAuth();
   const navigate = useNavigate();
 
   const handleLogin = () => {
     navigate('/login');
   };
-
-  const handleButtonClick = () => {
-    handleLogin();
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${process.env.REACT_APP_API_URL}/user/logout`);
+      window.alert('로그아웃에 성공했습니다');
+      logout();
+    } catch (error) {
+      console.error('로그아웃 실패:', error);
+    }
+    navigate('/'); // 로그아웃 후 홈페이지로 이동
   };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -25,9 +34,8 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             KUUK[꾹!]
           </Typography>
-          <Typography variant="subtitle1">환영합니다</Typography>
-          <Button variant="outlined" color="inherit" onClick={handleButtonClick}>
-            Login
+          <Button variant="outlined" color="inherit" onClick={isLoggedIn ? handleLogout : handleLogin}>
+            {isLoggedIn ? 'Logout' : 'Login'}
           </Button>
         </Toolbar>
       </AppBar>
