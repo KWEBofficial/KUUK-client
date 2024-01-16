8b0fd001145ba756e0411a253881261e3f3cf7f3
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 <sue.gus77@gmail.com>
2024-01-16 15:42:21 +0900

    MOD: header 컬러 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -37,7 +37,7 @@ export default function Header() {
   }
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: '#FEA82F' }}>
         <Toolbar>
           <SideBar />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
