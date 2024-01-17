c58e4a10497777aec061abd3185ea2543891fa02
박보건 <68377988+cympyc1785@users.noreply.github.com>
2024-01-17 23:12:25 +0900

    MOD: 오류 처리 및 일부 내용 수정

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -18,13 +18,15 @@ export function HistoryPage() {
       if (status === 200) {
         setHistories(response);
       } else {
-        throw new Error();
+        console.error('히스토리 정보를 가져오는데 실패했습니다.');
+        navigate('/');
       }
     } catch (e) {
       console.error('히스토리 정보를 가져오는데 실패했습니다.');
       if (axios.isAxiosError(e)) {
         window.alert(e.response?.data.message);
       }
+      navigate('/login');
     }
   }
 
@@ -37,6 +39,11 @@ export function HistoryPage() {
       <Box paddingX={8} paddingY={3}>
         <Typography variant="h4">히스토리</Typography>
       </Box>
+      {histories.length === 0 && (
+        <Box padding="40px">
+          <Typography>히스토리가 없습니다!</Typography>
+        </Box>
+      )}
       <Box paddingX={8} paddingY={2} sx={{ maxHeight: '70vh', overflowY: 'auto' }}>
         <Grid container spacing={4} paddingX={3}>
           {histories.map((history) => (
@@ -58,7 +65,7 @@ export function HistoryPage() {
           right: '16px',
         }}
       >
-        <Button variant="contained" onClick={() => navigate('/')}>
+        <Button variant="contained" onClick={() => navigate('/poll')}>
           투표 시작하기
         </Button>
       </Box>
