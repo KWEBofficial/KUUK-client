7aed6bbf2fc92c9e31682f4eb97aebc4123e6b0f
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 <sue.gus77@gmail.com>
2024-01-17 16:42:17 +0900

    MOD: fontWeight, textColor 수정

--- src/components/customBubble/index.tsx
+++ src/components/customBubble/index.tsx
@@ -10,6 +10,8 @@ interface CustomBubbleProps {
   tailFlip?: boolean; // 꼬리 뒤집기 여부
   backgroundColor?: string;
   textAlign?: 'left' | 'right' | 'center'; // 배경 색상
+  textColor?: string;
+  fontWeight?: number | 'bold' | 'normal' | 'bolder' | 'lighter';
 }
 
 export const CustomBubble: React.FC<CustomBubbleProps> = ({
@@ -18,32 +20,36 @@ export const CustomBubble: React.FC<CustomBubbleProps> = ({
   width = 'auto',
   height = 'auto',
   tailFlip = false,
-  backgroundColor = '#FEA82F',
-  textAlign = 'left', // 기본 색상 설정
+  backgroundColor = 'primary.main',
+  textAlign = 'left',
+  textColor = 'primary.contrastText',
+  fontWeight = 'bold',
 }) => {
   return (
     <Paper
       sx={{
         padding: 2,
         position: 'relative',
-        backgroundColor, // 배경 색상 사용
+        backgroundColor,
         borderRadius: '16px',
-        width, // 너비 설정
-        height, // 높이 설정
+        width,
+        height,
         textAlign,
+        color: textColor,
+        fontWeight,
         '&:after': {
           content: '""',
           position: 'absolute',
           bottom: 0,
-          left: tailPosition, // 꼬리 위치 조정
+          left: tailPosition,
           width: 0,
           height: 0,
           border: '10px solid transparent',
-          borderTopColor: tailFlip ? 'transparent' : backgroundColor, // 여기를 변경
-          borderBottom: 'none', // 여기를 변경
+          borderTopColor: tailFlip ? 'transparent' : backgroundColor,
+          borderBottom: 'none',
           borderLeft: tailFlip ? 'none' : '10px solid transparent',
-          borderRight: tailFlip ? '10px solid transparent' : 'none', // 여기를 변경
-          borderRightColor: tailFlip ? backgroundColor : 'transparent', // 여기를 변경
+          borderRight: tailFlip ? '10px solid transparent' : 'none',
+          borderRightColor: tailFlip ? backgroundColor : 'transparent',
           transform: tailFlip ? 'translateY(100%) rotateZ(180deg)' : 'translateY(100%)',
         },
       }}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,34 +1,37 @@
 import { useNavigate } from 'react-router-dom';
-import { Box, Typography, Grid, Button, Stack } from '@mui/material';
+import { Box, Typography, Grid, Stack } from '@mui/material';
 
-import { CustomBubble } from '../../components/customBubble';
+import CustomButton from '../../components/CustomButton';
+import { CustomBubble } from '../../components/CustomBubble';
 
 export function MainPage() {
   const navigate = useNavigate();
 
   return (
+    /* 스크롤 바 */
     <Box
       sx={{
         flexGrow: 1,
         maxHeight: '650px',
         overflowY: 'scroll',
         '&::-webkit-scrollbar': {
-          width: '5px', // 스크롤바 너비 설정
+          width: '5px',
         },
         '&::-webkit-scrollbar-thumb': {
-          backgroundColor: 'transparent', // 스크롤바 썸을 투명하게 설정
+          backgroundColor: 'transparent',
         },
         '&:hover::-webkit-scrollbar-thumb': {
           backgroundColor: 'rgba(0, 0, 0, 0.2)',
-          borderRadius: '5px', // 마우스 호버 시 스크롤바 썸 색상 설정
+          borderRadius: '5px',
         },
       }}
     >
+      {/* 스크롤 바 */}
       <Grid container spacing={0}>
         {/* 좌측 서비스 설명 */}
         <Grid item xs={12} md={6}>
           <Stack spacing={3}>
-            <Typography textAlign="center" variant="h4" gutterBottom>
+            <Typography textAlign="center" variant="h4" gutterBottom paddingTop={3}>
               당신의 선택으로 KUUK[꾹]!
             </Typography>
             <Box
@@ -36,7 +39,7 @@ export function MainPage() {
               pr="130px"
               sx={{
                 '& > * + *': {
-                  marginTop: 2, // 자식 컴포넌트 간의 상단 마진
+                  marginTop: 2,
                 },
               }}
             >
@@ -46,7 +49,7 @@ export function MainPage() {
                 </CustomBubble>
               </Box>
               <Box display="flex" justifyContent={'flex-end'}>
-                <CustomBubble textAlign="right" tailPosition="85%" backgroundColor="#FFC288">
+                <CustomBubble textAlign="right" tailPosition="85%" backgroundColor="primary.light">
                   그러게 뭐 먹지?
                 </CustomBubble>
               </Box>
@@ -61,7 +64,7 @@ export function MainPage() {
                 </CustomBubble>
               </Box>
               <Box display="flex" justifyContent={'flex-end'}>
-                <CustomBubble textAlign="right" tailPosition="85%" backgroundColor="#FFC288">
+                <CustomBubble textAlign="right" tailPosition="85%" backgroundColor="primary.light">
                   A식당은 어때?
                 </CustomBubble>
               </Box>
@@ -71,7 +74,7 @@ export function MainPage() {
                 </CustomBubble>
               </Box>
               <Box display="flex" justifyContent={'flex-end'}>
-                <CustomBubble textAlign="right" tailPosition="89%" backgroundColor="#FFC288">
+                <CustomBubble textAlign="right" tailPosition="89%" backgroundColor="primary.light">
                   그냥 우리 밥 먹지 말까?
                 </CustomBubble>
               </Box>
@@ -96,24 +99,18 @@ export function MainPage() {
               height: '100%',
             }}
           >
-            <Box>
-              <Typography>꾹이란?</Typography>
+            <Box padding={10}>
+              <Typography marginBottom={1} variant="h3" fontWeight="bold">
+                꾹이란?
+              </Typography>
               <Typography>
-                식당을 고르는 것을 어려워하는 여러분꼐, 점심 메뉴를 고르다 의가 상할 뻔한 당신에게 필요한 서비스 KUUK
+                식당을 고르는 것을 어려워하는 여러분께, 점심 메뉴를 고르다 의 상할 뻔한 당신에게 필요한 서비스 KUUK
               </Typography>
             </Box>
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              sx={{ marginBottom: 2 }}
-              onClick={() => navigate('/')}
-            >
-              투표생성하기
-            </Button>
-            <Button variant="contained" color="secondary" size="large">
-              이전 투표 둘러보기
-            </Button>
+            <Stack spacing={2}>
+              <CustomButton text="투표방 생성하기" onClick={() => navigate('/poll')} />
+              <CustomButton text="이전 투표 둘러보기" onClick={() => navigate('/history')} />
+            </Stack>
           </Stack>
         </Grid>
       </Grid>
