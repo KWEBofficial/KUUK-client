78347ef46ef2b6dacb2f9b29ef007dd434a69ee8
strn18 <strn18@korea.ac.kr>
2024-01-17 17:29:27 +0900

    MOD: 커스텀 버튼 가로 세로 길이 prop 추가

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -3,11 +3,13 @@ import { Button } from '@mui/material';
 interface CustomButtonProps {
   text: string;
   onClick: (() => Promise<void>) | (() => void);
+  width?: string | number;
+  height?: string | number;
 }
 
-export default function CustomButton({ text, onClick }: CustomButtonProps) {
+export default function CustomButton({ text, onClick, width, height }: CustomButtonProps) {
   return (
-    <Button variant="contained" color="primary" onClick={onClick}>
+    <Button variant="contained" color="primary" onClick={onClick} sx={{ width, height }}>
       {text}
     </Button>
   );
