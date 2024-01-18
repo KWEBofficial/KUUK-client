418894af2bd3818a00f5c3d524076c84116bd8de
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 <sue.gus77@gmail.com>
2024-01-18 12:13:14 +0900

    ADD: react-toastify 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 
@@ -6,6 +7,7 @@ import { Layout } from './components/Layout';
 import './App.css';
 import { AuthProvider } from './components/AuthContent';
 
+import 'react-toastify/dist/ReactToastify.css';
 /**
  * mui에서 제공하는 테마 설정입니다.
  * 글꼴을 Pretendard로 설정했습니다.
@@ -40,6 +42,7 @@ function App() {
           </Layout>
         </AuthProvider>
       </BrowserRouter>
+      <ToastContainer />
     </ThemeProvider>
   );
 }
