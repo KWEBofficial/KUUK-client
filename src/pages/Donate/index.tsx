d794029121029cfc28ebb5c8809d4a51ee8cc3c9
strn18 <strn18@korea.ac.kr>
2024-01-18 15:16:28 +0900

    MOD: 후원 페이지 구체적으로 디자인

--- src/pages/Donate/index.tsx
+++ src/pages/Donate/index.tsx
@@ -1,10 +1,92 @@
-import { Box, Typography } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 
 export function DonatePage() {
   return (
-    <Box>
-      <Typography>아래 계좌로 후원</Typography>
-      <Typography>793-910370-23607 하나 문정민</Typography>
+    <Box padding={5}>
+      <Box marginTop={10} paddingX={10} display={'flex'} justifyContent={'center'}>
+        <IntroduceCard
+          imgDir="/img/1.jpg"
+          name="김수현"
+          roles={['프론트엔드', '백엔드', '디자인']}
+          backgroundColor="white"
+        />
+        <IntroduceCard
+          imgDir="/img/2.jpg"
+          name="문정민"
+          roles={['프론트엔드', '백엔드', '디자인']}
+          backgroundColor="white"
+        />
+        <IntroduceCard
+          imgDir="/img/3.jpg"
+          name="박보건"
+          roles={['프론트엔드', '백엔드', '디자인']}
+          backgroundColor="white"
+        />
+        <IntroduceCard
+          imgDir="/img/4.jpg"
+          name="정연승"
+          roles={['프론트엔드', '백엔드', '디자인']}
+          backgroundColor="white"
+        />
+        <IntroduceCard
+          imgDir="/img/5.jpg"
+          name="정혜민"
+          roles={['프론트엔드', '백엔드', '디자인']}
+          backgroundColor="white"
+        />
+      </Box>
+    </Box>
+  );
+}
+
+interface IntroduceCardProps {
+  imgDir: string;
+  name: string;
+  roles: string[];
+  backgroundColor: string;
+}
+
+function IntroduceCard({ imgDir, name, roles, backgroundColor }: IntroduceCardProps) {
+  return (
+    <Box
+      width={'20%'}
+      display={'flex'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      sx={{ backgroundColor: { backgroundColor } }}
+    >
+      <Avatar alt={name} src={imgDir} sx={{ width: 120, height: 120 }} />
+      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} width={'100%'} height={'70px'}>
+        <Typography variant="h6" align="center">
+          {name}
+        </Typography>
+      </Box>
+      <Box
+        paddingX={1}
+        display={'flex'}
+        justifyContent={'center'}
+        flexWrap={'wrap'}
+        gap={2}
+        sx={{ backgroundColor: 'white' }}
+      >
+        {roles.map((role) => (
+          <Typography
+            padding={'6px'}
+            paddingX={'12px'}
+            display="inline"
+            align="center"
+            borderRadius={8}
+            sx={{ backgroundColor: 'primary.light' }}
+          >
+            {role}
+          </Typography>
+        ))}
+      </Box>
+      <Box marginY={4}>
+        <a href="https://gift.kakao.com/product/5417137" target="_blank">
+          <button>후원하기</button>
+        </a>
+      </Box>
     </Box>
   );
 }
